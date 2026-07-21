cec0f3b9219a206a8791f648752a2a29b6e5f864
Md Shariful Islam <mdsharifulkawsar@gmail.com>
2026-07-21 23:23:11 +0600

    feat: create health profile edit form with biometric and fitness goal inputs

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Update Health Profile') }}
+            {{ __('UPDATE YOUR HEALTH PROFILE') }}
         </h2>
     </x-slot>
 
